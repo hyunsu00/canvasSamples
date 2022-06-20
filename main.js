26c375043cdb8a4f9d5ce717a1177d957995a854
hnc-hyunsu00 <hyunsu00@hancom.com>
2022-06-20 18:56:22 +0900

    초기업로드

---
+++ main.js
@@ -0,0 +1,29 @@
+window.onload = function () {
+	// 캔버스
+	let canvas = document.getElementById("canvas");
+	let ctx  = canvas.getContext("2d");
+	ctx .font = "36px Helvetica";
+	ctx.fillStyle = "red";
+	ctx .fillText('김현수', 0, 40);
+	// 이미지
+	let img = document.getElementById("exportedImage");
+	img.src = canvas.toDataURL('image/png');
+
+};
+
+// 다운로드
+function download() {
+	// let download = document.getElementById("download");
+	// let image = document.getElementById("canvas").toDataURL("image/png")
+	// 						.replace("image/png", "image/octet-stream");
+	// download.setAttribute("href", image);
+
+	const { jsPDF } = window.jspdf;
+	const doc = new jsPDF(
+		{filters: ['FlateEncode']}
+	);
+
+	// doc.addImage('./image.png', 0, 0, 129, 54);
+	doc.addImage(document.getElementById("canvas").toDataURL("image/png"), 0, 0, 129, 54);
+	doc.save("a4.pdf");
+}
