c4ad69a6375a023456b283fd14ba64d406511f47
hyunsu00 <hyun955807@naver.com>
2022-06-22 09:25:47 +0900

    코드 수정

--- main.js
+++ main.js
@@ -18,12 +18,27 @@ function download() {
 	// 						.replace("image/png", "image/octet-stream");
 	// download.setAttribute("href", image);
 
-	const { jsPDF } = window.jspdf;
-	const doc = new jsPDF(
-		{filters: ['FlateEncode']}
-	);
+	// const { jsPDF } = window.jspdf;
+	// const doc = new jsPDF(
+	// 	{filters: ['FlateEncode']}
+	// );
 
 	// doc.addImage('./image.png', 0, 0, 129, 54);
-	doc.addImage(document.getElementById("canvas").toDataURL("image/png"), 0, 0, 129, 54);
-	doc.save("a4.pdf");
+	// doc.addImage(document.getElementById("canvas").toDataURL("image/png"), 0, 0, 129, 54);
+	// doc.save("a4.pdf");
+
+	var canvas = document.createElement("canvas");                               
+	document.body.appendChild(canvas);                                           
+	canvas.width = 129;                                            
+	canvas.height = 54;                                                         
+	var ctx = canvas.getContext("2d");                                           
+	ctx .font = "36px Helvetica";
+	ctx.fillStyle = "red";
+	ctx.fillText('김현수', 0, 40);
+			
+	const { jsPDF } = window.jspdf;
+	var doc = new jsPDF({filters: ['FlateEncode']});
+
+	doc.addImage(canvas.toDataURL("image/png"), 0, 0, 129, 54);
+	doc.save("png.pdf");
 }
